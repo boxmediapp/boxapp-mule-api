3ec7b77bf7bb570ba61b2e92dad81580e0640386
Dilshat Hewzulla <hewzulla@gmail.com>
2017-07-10 11:09:26 +0100

    artist and tract to metadata

--- deploy/deploy_to_box.sh
+++ deploy/deploy_to_box.sh
@@ -1,3 +1,3 @@
 source /Users/dilshathewzulla/box/box-secrets/box.sh
-echo "deploying the version 3.0.7-SNAPSHOT to ec2-user@boxnetwork.co.uk using the property file /Users/dilshathewzulla/box/box-secrets/box.sh (for replacement of the environment specific variables) ..."
-deploy/deploy.sh boxnetwork.co.uk ec2-user 3.0.7-SNAPSHOT
+echo "deploying the version 3.0.7 to ec2-user@boxnetwork.co.uk using the property file /Users/dilshathewzulla/box/box-secrets/box.sh (for replacement of the environment specific variables) ..."
+deploy/deploy.sh boxnetwork.co.uk ec2-user 3.0.7
