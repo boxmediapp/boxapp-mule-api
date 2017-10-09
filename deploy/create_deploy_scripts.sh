b4347a54e209b9688c59f1a3011f0ad9ff3ed86e
Dilshat Hewzulla <hewzulla@gmail.com>
2017-10-09 23:39:56 +0100

    added image

--- deploy/create_deploy_scripts.sh
+++ deploy/create_deploy_scripts.sh
@@ -4,6 +4,6 @@ createDeployScript dubuntu  $projectversion  ~/box/box-secrets/box.sh  dubuntu  
 createDeployScript dev      $projectversion  ~/box/box-secrets/box.sh  userver              davran
 createDeployScript box      $projectversion  ~/box/box-secrets/box.sh  boxnetwork.co.uk     ec2-user
 createDeployScript bebox    $projectversion  ~/box/box-secrets/bebox.sh  be.boxnetwork.co.uk  ec2-user
-
+createDeployScript image    $projectversion  ~/box/box-secrets/image.sh  image.boxnetwork.co.uk  ec2-user
 
 
