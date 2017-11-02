90a0fbf1358625114167ed9e8c6f6db68f8b0322
Dilshat Hewzulla <hewzulla@gmail.com>
2017-11-02 21:57:38 +0000

    releasing

--- deploy/deploy_to_image.sh
+++ deploy/deploy_to_image.sh
@@ -1,3 +1,3 @@
 source /Users/dilshathewzulla/box/box-secrets/image.sh
-echo "deploying the version 3.0.12-SNAPSHOT to ec2-user@image.boxnetwork.co.uk using the property file /Users/dilshathewzulla/box/box-secrets/image.sh (for replacement of the environment specific variables) ..."
-deploy/deploy.sh image.boxnetwork.co.uk ec2-user 3.0.12-SNAPSHOT
+echo "deploying the version 3.0.13-SNAPSHOT to ec2-user@image.boxnetwork.co.uk using the property file /Users/dilshathewzulla/box/box-secrets/image.sh (for replacement of the environment specific variables) ..."
+deploy/deploy.sh image.boxnetwork.co.uk ec2-user 3.0.13-SNAPSHOT
--- src/main/java/uk/co/boxnetwork/mule/transformers/images/AllClientImageTransformer.java
+++ src/main/java/uk/co/boxnetwork/mule/transformers/images/AllClientImageTransformer.java
@@ -1,19 +1,20 @@
 package uk.co.boxnetwork.mule.transformers.images;
 
 import org.mule.api.MuleMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 
 import uk.co.boxnetwork.components.ImageService;
 import uk.co.boxnetwork.data.SearchParam;
-import uk.co.boxnetwork.data.image.Image;
-import uk.co.boxnetwork.data.image.ImageSet;
 import uk.co.boxnetwork.model.AppConfig;
 import uk.co.boxnetwork.mule.transformers.BoxRestTransformer;
 import uk.co.boxnetwork.mule.util.MuleRestUtil;
 
 public class AllClientImageTransformer extends BoxRestTransformer{
+	static final protected Logger logger=LoggerFactory.getLogger(AllClientImageTransformer.class);
 	@Autowired
 	ImageService imageService;
 	
@@ -21,14 +22,77 @@ public class AllClientImageTransformer extends BoxRestTransformer{
 	@Autowired
 	AppConfig appConfig;
 
+	
+	private String[] clientIds={"virgin","freesat","ee","sky","freeview"};
 	@Override
 	protected Object processGET(MuleMessage message, String outputEncoding){				
-		String imgid=MuleRestUtil.getPathPath(message);
-		if(imgid==null || imgid.length()==0){
+		String path=MuleRestUtil.getPathPath(message);
+		
+		if(path==null || path.length()==0){
 			return findImages(message,outputEncoding);
 			}
 		else{
-			return imageService.findClientImageById(Long.valueOf(imgid));					
+			   int ib=path.indexOf("/");
+			   String clientIdPath=null;
+			   String imgid=null;
+			   if(ib==0){
+				   return returnError("wrong path", message);
+			   }
+			   else if(ib>0){
+				   clientIdPath=path.substring(0,ib);	
+				   if((ib+1)<path.length()){
+					   imgid=path.substring(ib+1);
+				   }
+			   }
+			   else{
+				   clientIdPath=path;
+			   }
+			   String clientId=null;
+			   for(String id:clientIds){
+				   if(id.equals(clientIdPath)){
+					   clientId=id;
+					   break;
+				   }
+			   }
+			   if(clientId==null){
+				   if(imgid!=null){
+					   return returnError("wrong client id:"+path, message);
+				   }
+				   else{
+					   Long imid=null;
+					   try{
+						   imid=Long.valueOf(clientIdPath);
+						   return imageService.findClientImageById(imid);
+						   
+					   }
+					   catch(Exception e){
+						   return returnError("wrong client id"+path, message);
+					   }
+				   }
+				   
+			   }
+			   else{
+				   return findImages(clientId,imgid,message,outputEncoding);
+			   }
+			   
+			   
+								
+		}
+	}
+	private Object findImages(String clientid,String imgid,MuleMessage message, String outputEncoding){
+		logger.info("Processing client image request clientid=["+clientid+"]imgid=["+imgid+"]");
+		if(imgid==null){
+			return findImages(message,outputEncoding);
+		}
+		else{
+			try{
+				  Long imid=Long.valueOf(imgid);
+				   return imageService.findClientImageById(imid);
+				   
+			   }
+			   catch(Exception e){
+				   return returnError("wrong imgid id"+imgid, message);
+			   }
 		}
 	}
 	
