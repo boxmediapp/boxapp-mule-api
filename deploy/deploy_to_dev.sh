97bf3e451c9f5adbe0ff8bda1973884cc7500267
Dilshat Hewzulla <hewzulla@gmail.com>
2017-11-04 16:20:41 +0000

    releasing

--- deploy/deploy_to_dev.sh
+++ deploy/deploy_to_dev.sh
@@ -1,3 +1,3 @@
 source /Users/dilshathewzulla/box/box-secrets/box.sh
-echo "deploying the version 3.0.15 to davran@userver using the property file /Users/dilshathewzulla/box/box-secrets/box.sh (for replacement of the environment specific variables) ..."
-deploy/deploy.sh userver davran 3.0.15
+echo "deploying the version 3.0.17-SNAPSHOT to davran@userver using the property file /Users/dilshathewzulla/box/box-secrets/box.sh (for replacement of the environment specific variables) ..."
+deploy/deploy.sh userver davran 3.0.17-SNAPSHOT
