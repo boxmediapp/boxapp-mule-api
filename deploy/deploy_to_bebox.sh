892ba021306f351c79869ebd8b254d5561febe97
Dilshat Hewzulla <hewzulla@gmail.com>
2017-11-14 22:53:42 +0000

    releasing

--- deploy/deploy_to_bebox.sh
+++ deploy/deploy_to_bebox.sh
@@ -1,3 +1,3 @@
 source /Users/dilshathewzulla/box/box-secrets/bebox.sh
-echo "deploying the version 3.0.18-SNAPSHOT to ec2-user@bemediaapp.iterativesolution.co.uk using the property file /Users/dilshathewzulla/box/box-secrets/bebox.sh (for replacement of the environment specific variables) ..."
-deploy/deploy.sh bemediaapp.iterativesolution.co.uk ec2-user 3.0.18-SNAPSHOT
+echo "deploying the version 3.0.18 to ec2-user@bemediaapp.iterativesolution.co.uk using the property file /Users/dilshathewzulla/box/box-secrets/bebox.sh (for replacement of the environment specific variables) ..."
+deploy/deploy.sh bemediaapp.iterativesolution.co.uk ec2-user 3.0.18
