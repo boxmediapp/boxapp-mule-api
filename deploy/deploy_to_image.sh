d702f3cb0a694fafb21f9eb53b801510b1ed1b3a
Dilshat Hewzulla <hewzulla@gmail.com>
2017-12-18 22:31:53 +0000

    releasing

--- deploy/deploy_to_image.sh
+++ deploy/deploy_to_image.sh
@@ -1,3 +1,3 @@
 source /Users/dilshathewzulla/box/box-secrets/image.sh
-echo "deploying the version 3.1.3 to ec2-user@image.boxnetwork.co.uk using the property file /Users/dilshathewzulla/box/box-secrets/image.sh (for replacement of the environment specific variables) ..."
-deploy/deploy.sh image.boxnetwork.co.uk ec2-user 3.1.3
+echo "deploying the version 3.1.4 to ec2-user@image.boxnetwork.co.uk using the property file /Users/dilshathewzulla/box/box-secrets/image.sh (for replacement of the environment specific variables) ..."
+deploy/deploy.sh image.boxnetwork.co.uk ec2-user 3.1.4
