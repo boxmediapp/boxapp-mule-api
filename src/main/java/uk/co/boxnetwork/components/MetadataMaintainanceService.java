9e4990cf4971c7d6ffe4428753d5096e9ed17631
Dilshat Hewzulla <hewzulla@gmail.com>
2017-12-25 10:07:31 +0000

    admin login duration is now 6 min

--- src/main/java/uk/co/boxnetwork/components/MetadataMaintainanceService.java
+++ src/main/java/uk/co/boxnetwork/components/MetadataMaintainanceService.java
@@ -899,7 +899,7 @@ public void calculateUploadedDuration(){
    }
    public void createBoxUserRoles(){	
 	   logger.info("**************start to create roles***********");
-	   repository.createBoxUserRole(new BoxUserRole("admin",      "ROLE_ADMIN","admin",   Long.valueOf(3600)));	   
+	   repository.createBoxUserRole(new BoxUserRole("admin",      "ROLE_ADMIN","admin",   Long.valueOf(360)));	   
 	   repository.createBoxUserRole(new BoxUserRole("operator",   "ROLE_OPERATOR","full-access",    Long.valueOf(3600)));
 	   repository.createBoxUserRole(new BoxUserRole("readonly-operator",   "ROLE_OPERATOR","readonly-operator",    Long.valueOf(3600)));
 	   repository.createBoxUserRole(new BoxUserRole("client",     "ROLE_CLIENT", "client-access", Long.valueOf(3600)));
