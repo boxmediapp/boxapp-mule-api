ce2eacb6227743483aac05eec4dc63cc391008fd
Dilshat Hewzulla <hewzulla@gmail.com>
2018-03-04 15:38:25 +0000

    4.2

--- deploy/deploy_to_dubuntu.sh
+++ deploy/deploy_to_dubuntu.sh
@@ -1,3 +1,3 @@
 source /Users/dilshathewzulla/box/box-secrets/box.sh
-echo "deploying the version 4.0.13 to dilshat@dubuntu using the property file /Users/dilshathewzulla/box/box-secrets/box.sh (for replacement of the environment specific variables) ..."
-deploy/deploy.sh dubuntu dilshat 4.0.13
+echo "deploying the version 4.1.0-SNAPSHOT to dilshat@dubuntu using the property file /Users/dilshathewzulla/box/box-secrets/box.sh (for replacement of the environment specific variables) ..."
+deploy/deploy.sh dubuntu dilshat 4.1.0-SNAPSHOT
